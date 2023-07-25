7b9bef8b7fe27bf63c65280effbc62a1a5183b23
DragonnZhang <731557579@qq.com>
2023-07-25 21:02:07 +0800

    added comment

--- pages/[...all].vue
+++ pages/[...all].vue
@@ -4,5 +4,7 @@ router.push('/')
 </script>
 
 <template>
+  <!-- This page is used to redirect to homepage if a route is not caught. So in fact template is useless for this
+    component. -->
   <div></div>
 </template>
