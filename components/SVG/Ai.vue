0f6b4faff9b23819d05f5f3c28544e72f52b3ab5
DragonnZhang <731557579@qq.com>
2023-10-09 00:16:34 +0800

    feat: added new svg and refactored old svg

--- components/SVG/Ai.vue
+++ components/SVG/Ai.vue
@@ -1,11 +1,15 @@
 <template>
-  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" data-v-d00cfa20=""
-    class="">
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" data-v-d00cfa20="" class="">
     <rect x="1" y="8" width="1.3" height="5" rx="0.5" fill="currentColor" data-v-d00cfa20=""></rect>
-  <rect x="14" y="8" width="1.3" height="5" rx="0.5" fill="currentColor" data-v-d00cfa20=""></rect>
-  <rect x="7.5" y="4" width="1" height="3" rx="0.5" fill="currentColor" data-v-d00cfa20=""></rect>
-  <circle cx="8" cy="2" r="1" fill="currentColor" data-v-d00cfa20=""></circle>
-  <path fill-rule="evenodd" clip-rule="evenodd"
-    d="M3 7C3 6.44772 3.44772 6 4 6H12C12.5523 6 13 6.44772 13 7V14C13 14.5523 12.5523 15 12 15H4C3.44772 15 3 14.5523 3 14V7ZM7 10.75C7 11.1642 6.66421 11.5 6.25 11.5C5.83579 11.5 5.5 11.1642 5.5 10.75C5.5 10.3358 5.83579 10 6.25 10C6.66421 10 7 10.3358 7 10.75ZM9.75 11.5C10.1642 11.5 10.5 11.1642 10.5 10.75C10.5 10.3358 10.1642 10 9.75 10C9.33579 10 9 10.3358 9 10.75C9 11.1642 9.33579 11.5 9.75 11.5Z"
-    fill="currentColor" data-v-d00cfa20=""></path>
-</svg></template>
+    <rect x="14" y="8" width="1.3" height="5" rx="0.5" fill="currentColor" data-v-d00cfa20=""></rect>
+    <rect x="7.5" y="4" width="1" height="3" rx="0.5" fill="currentColor" data-v-d00cfa20=""></rect>
+    <circle cx="8" cy="2" r="1" fill="currentColor" data-v-d00cfa20=""></circle>
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M3 7C3 6.44772 3.44772 6 4 6H12C12.5523 6 13 6.44772 13 7V14C13 14.5523 12.5523 15 12 15H4C3.44772 15 3 14.5523 3 14V7ZM7 10.75C7 11.1642 6.66421 11.5 6.25 11.5C5.83579 11.5 5.5 11.1642 5.5 10.75C5.5 10.3358 5.83579 10 6.25 10C6.66421 10 7 10.3358 7 10.75ZM9.75 11.5C10.1642 11.5 10.5 11.1642 10.5 10.75C10.5 10.3358 10.1642 10 9.75 10C9.33579 10 9 10.3358 9 10.75C9 11.1642 9.33579 11.5 9.75 11.5Z"
+      fill="currentColor"
+      data-v-d00cfa20=""
+    ></path>
+  </svg>
+</template>
