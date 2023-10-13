a1d7589fb604bf5cbfd454461d5ef7666c08f0c4
DragonnZhang <731557579@qq.com>
2023-10-13 22:30:00 +0800

    ref: logic extracted to mainview

--- components/MainView/MainView.vue
+++ components/MainView/MainView.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import MainViewLeftSideBar from '~/components/MainView/MainViewLeftSideBar.vue'
 import MainViewBody from '~/components/MainView/MainViewBody.vue'
+import MainHeader from '~/components/MainView/MainHeader.vue'
 
 const leftBarData = ref([
   {
@@ -44,13 +45,57 @@ const leftBarData = ref([
     url: '/article'
   }
 ])
+
+const data = ref([
+  {
+    text: '首页',
+    url: '/',
+    relate: ['/', '/following', '/recommended', '/backend', '/frontend', '/android', '/ios', '/ai', '/freebie', '/career', '/article']
+  },
+  {
+    text: '沸点',
+    url: '/pins'
+  },
+  {
+    text: '课程',
+    url: '/course'
+  },
+  {
+    text: '直播',
+    url: '/live'
+  },
+  {
+    text: '活动',
+    url: '/events/all'
+  },
+  {
+    text: '竞赛',
+    url: '/challenge'
+  },
+  {
+    text: '商城',
+    url: ''
+  },
+  {
+    text: 'APP',
+    url: ''
+  },
+  {
+    text: '插件',
+    url: ''
+  }
+])
+const url = ref('https://p3-passport.byteimg.com/img/user-avatar/4d142fd37c164eb5069cab0af2bd8a03~100x100.awebp')
 </script>
 
 <template>
-  <div class="view timeline-index-view">
-    <MainViewLeftSideBar :data="leftBarData" />
-    <MainViewBody />
-  </div>
+  <MainHeader :data="data" :avatar-u-r-l="url" />
+  <main class="container main-container with-view-nav">
+    <div class="view timeline-index-view">
+      <MainViewLeftSideBar :data="leftBarData" />
+      <MainViewBody />
+    </div>
+  </main>
 </template>
 
 <style scoped lang="scss">
@@ -61,5 +106,21 @@ const leftBarData = ref([
 .timeline-index-view {
   display: flex;
   flex-direction: row;
+}
+
+main.container {
+  max-width: 1200px;
+}
+
+@media (max-width: 1220px) {
+  main.container {
+    max-width: 1000px;
+  }
+}
+
+.container {
+  position: relative;
+  margin: 0 auto;
+  width: 100%;
 }
 </style>
---
+++ pages/post/[id].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const { id } = route.params
+console.log(id)
+</script>
+
+<template>
+  <div></div>
+</template>
