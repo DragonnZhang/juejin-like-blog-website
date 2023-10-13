731196f9aae563f12b99268623b9beefd0f10db7
DragonnZhang <731557579@qq.com>
2023-10-14 01:34:38 +0800

    feat: added editor header

---
+++ components/Editor/EditorHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="header editor-header">
+    <input placeholder="输入文章标题..." spellcheck="false" maxlength="80" class="title-input" />
+    <div class="right-box"></div>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+  display: flex;
+  align-items: center;
+  padding: 0 27px;
+  height: 5.334rem;
+  background-color: var(--juejin-background-color);
+  border-bottom: 1px solid #ddd;
+
+  .title-input {
+    -webkit-box-flex: 1;
+    -ms-flex: 1 1 auto;
+    flex: 1 1 auto;
+    height: 100%;
+    background-color: var(--juejin-background-color);
+
+    margin: 0;
+    padding: 0;
+    font-size: 24px;
+    font-weight: 500;
+    color: var(--juejin-font-white);
+    border: none;
+    outline: none;
+  }
+}
+</style>
