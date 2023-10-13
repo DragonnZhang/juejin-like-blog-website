bae00d551d1cb0e542c624afb42c4ec073a0393e
DragonnZhang <731557579@qq.com>
2023-10-14 01:34:49 +0800

    feat: added editor main

---
+++ components/Editor/EditorMain.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import UploadButton from '~/components/Editor/UploadButton.vue'
+import { useArticle } from '~/composables/states'
+import { marked } from 'marked'
+
+const article = useArticle()
+const preview = computed(() => marked.parse(article.value))
+</script>
+
+<template>
+  <div class="main"></div>
+</template>
+
+<style scoped lang="scss"></style>
