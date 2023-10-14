2672bf4ad8b51130f8bec327704c9523773e1c71
DragonnZhang <731557579@qq.com>
2023-10-14 17:12:41 +0800

    updated editor main

--- components/Editor/EditorMain.vue
+++ components/Editor/EditorMain.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
-import UploadButton from '~/components/Editor/UploadButton.vue'
+// import UploadButton from '~/components/Editor/UploadButton.vue'
 import { useArticle } from '~/composables/states'
-import { marked } from 'marked'
+import 'bytemd/dist/index.css'
+import 'highlight.js/styles/default.css'
+import zhHans from 'bytemd/lib/locales/zh_Hans.json'
+import highlight from '@bytemd/plugin-highlight-ssr'
+import { Editor } from '@bytemd/vue-next'
 
 const article = useArticle()
-const preview = computed(() => marked.parse(article.value))
+const plugins = ref([highlight()])
+
+function handleChange(v: string) {
+  article.value = v
+}
 </script>
 
 <template>
-  <div class="main"></div>
+  <div class="main">
+    <Editor :locale="zhHans" :plugins="plugins" :value="article" @change="handleChange" />
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style lang="scss" src="~/assets/stylesheets/editor.scss"></style>
