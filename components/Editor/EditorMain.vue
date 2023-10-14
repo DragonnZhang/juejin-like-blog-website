238e83de608301b0833b6f0cb61516c34412df1e
DragonnZhang <731557579@qq.com>
2023-10-14 20:50:01 +0800

    added frontmatter

--- components/Editor/EditorMain.vue
+++ components/Editor/EditorMain.vue
@@ -2,13 +2,15 @@
 // import UploadButton from '~/components/Editor/UploadButton.vue'
 import { useArticle } from '~/composables/states'
 import 'bytemd/dist/index.css'
-import 'highlight.js/styles/default.css'
+import 'highlight.js/styles/monokai-sublime.css'
+// import 'highlight.js/styles/github.css'
 import zhHans from 'bytemd/lib/locales/zh_Hans.json'
 import highlight from '@bytemd/plugin-highlight-ssr'
+import frontmatter from '@bytemd/plugin-frontmatter'
 import { Editor } from '@bytemd/vue-next'
 
 const article = useArticle()
-const plugins = ref([highlight()])
+const plugins = ref([highlight(), frontmatter()])
 
 function handleChange(v: string) {
   article.value = v
@@ -17,7 +19,7 @@ function handleChange(v: string) {
 
 <template>
   <div class="main">
-    <Editor :locale="zhHans" :plugins="plugins" :value="article" @change="handleChange" />
+    <Editor :plugins="plugins" :value="article" @change="handleChange" :locale="zhHans" />
   </div>
 </template>
 
