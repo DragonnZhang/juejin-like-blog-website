ab86a5fe4d098dc74d1e5632b1fc63155baffa38
DragonnZhang <731557579@qq.com>
2023-10-25 14:41:54 +0800

    feat: added post page

--- pages/post/[id].vue
+++ pages/post/[id].vue
@@ -1,10 +1,53 @@
 <script setup lang="ts">
-const route = useRoute()
+import MainHeader from '~/components/MainView/MainHeader.vue'
+import PostMain from '~/components/Post/PostMain.vue'
 
-const { id } = route.params
-console.log(id)
+const data = ref([
+  {
+    text: '首页',
+    url: '/',
+    relate: ['/', '/following', '/recommended', '/backend', '/frontend', '/android', '/ios', '/ai', '/freebie', '/career', '/article']
+  },
+  {
+    text: '沸点',
+    url: '/pins'
+  },
+  {
+    text: '课程',
+    url: '/course'
+  },
+  {
+    text: '直播',
+    url: '/live'
+  },
+  {
+    text: '活动',
+    url: '/events/all'
+  },
+  {
+    text: '竞赛',
+    url: '/challenge'
+  },
+  {
+    text: '商城',
+    url: ''
+  },
+  {
+    text: 'APP',
+    url: ''
+  },
+  {
+    text: '插件',
+    url: ''
+  }
+])
+const url = ref('https://p3-passport.byteimg.com/img/user-avatar/4d142fd37c164eb5069cab0af2bd8a03~100x100.awebp')
+
+const route = useRoute()
+const id = route.params.id as string
 </script>
 
 <template>
-  <div></div>
+  <MainHeader :data="data" :avatar-u-r-l="url" />
+  <PostMain :id="id" />
 </template>
