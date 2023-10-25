b244796f75829f5a51e7fb121e635e3788f966e2
DragonnZhang <731557579@qq.com>
2023-10-25 16:04:56 +0800

    added time icon

---
+++ components/SVG/Time.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="16" height="16" fill="none"></rect>
+    <circle cx="8" cy="8" r="5.65625" stroke="#8A919F"></circle>
+    <path d="M7.69141 5.18652V8.30924H10.8141" stroke="#8A919F" stroke-linecap="round" stroke-linejoin="round"></path>
+  </svg>
+</template>
