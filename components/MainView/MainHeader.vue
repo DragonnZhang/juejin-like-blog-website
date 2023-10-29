db86463336907d9d69a680e4f60e2ace3962bd10
DragonnZhang <731557579@qq.com>
2023-10-29 17:22:04 +0800

    fix: changed a to nuxtlink

--- components/MainView/MainHeader.vue
+++ components/MainView/MainHeader.vue
@@ -34,9 +34,9 @@ onBeforeUnmount(() => {
   <div class="main-header-box">
     <header class="main-header" :class="{ visible: !topVanish }">
       <div class="container">
-        <a href="/" class="logo">
+        <NuxtLink href="/" class="logo">
           <img src="~/assets/svg/logo.svg" alt="稀土掘金" class="logo-img" />
-        </a>
+        </NuxtLink>
         <nav role="navigation" class="main-nav">
           <ul class="nav-list">
             <li class="main-nav-list">
@@ -57,7 +57,7 @@ onBeforeUnmount(() => {
                   <li class="nav-item search"></li>
                   <li class="nav-item add creator-item">
                     <div class="add-group">
-                      <button class="add-btn">创作者中心</button>
+                      <button class="add-btn" @click="$router.push('/editor')">写文章</button>
                       <div class="more">
                         <svg width="12" height="12" viewBox="0 0 12 12" fill="none" class="unfold12-icon">
                           <path
@@ -79,7 +79,7 @@ onBeforeUnmount(() => {
                 </div>
               </li>
               <li class="nav-item notification">
-                <a href="/notification">
+                <NuxtLink href="/notification">
                   <svg width="18" height="21" viewBox="0 0 18 21" fill="none" class="notification-icon">
                     <path
                       d="M10.457 2.21484C10.457 1.38642 9.78546 0.714844 8.95703 0.714844C8.1286 0.714844 7.45703 1.38642 7.45703 2.21484V2.69324C6.2566 2.94818 5.19205 3.50792 4.33932 4.34674C3.13874 5.52773 2.41008 7.20991 2.26438 9.23124C2.13761 10.9899 1.87805 12.4178 0.441105 14.4515C-0.126181 15.2544 0.396205 16.4637 1.46658 16.4637H16.5354C17.6058 16.4637 18.1282 15.2544 17.5609 14.4515C16.124 12.4178 15.8644 10.9899 15.7376 9.23124C15.5919 7.20991 14.8633 5.52773 13.6627 4.34674C12.7892 3.48749 11.6934 2.92108 10.457 2.67516V2.21484Z"
@@ -88,7 +88,7 @@ onBeforeUnmount(() => {
                       d="M6.57422 17.3574C6.41191 17.3574 6.2597 17.4362 6.16599 17.5687C6.07227 17.7012 6.04871 17.871 6.1028 18.024C6.52421 19.2163 7.66123 20.0717 8.99933 20.0717C10.3374 20.0717 11.4745 19.2163 11.8959 18.024C11.9499 17.871 11.9264 17.7012 11.8327 17.5687C11.739 17.4362 11.5868 17.3574 11.4244 17.3574H6.57422Z"
                     ></path>
                   </svg>
-                </a>
+                </NuxtLink>
               </li>
               <li class="nav-item menu">
                 <div class="avatar-wrapper">
