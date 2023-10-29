a9d5f136325dc9091e8d17156df94e15d4db0573
DragonnZhang <731557579@qq.com>
2023-10-29 17:22:12 +0800

    added redirect page title

--- pages/[...all].vue
+++ pages/[...all].vue
@@ -4,6 +4,9 @@ router.push('/')
 </script>
 
 <template>
+  <Head>
+    <Title>juejin</Title>
+  </Head>
   <!-- This page is used to redirect to homepage if a route is not caught. So in fact template is useless for this
     component. -->
   <div></div>
