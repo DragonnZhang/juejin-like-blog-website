054fe6793b69d36bd9f8fd2addeb46f16ce415d1
DragonnZhang <731557579@qq.com>
2023-10-30 15:33:29 +0800

    added block button

---
+++ components/Public/BlockButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps<{
+  type: 'line' | 'primary'
+}>()
+</script>
+
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped lang="scss"></style>
