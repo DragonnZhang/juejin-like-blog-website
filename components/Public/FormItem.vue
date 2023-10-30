20aaa03df999f08000c6f92287b0e1c1c3681dad
DragonnZhang <731557579@qq.com>
2023-10-30 15:33:22 +0800

    added form item

---
+++ components/Public/FormItem.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+defineProps<{
+  label: string
+  required?: boolean
+}>()
+</script>
+
+<template>
+  <div class="form-item">
+    <div class="label" :class="{ required }">{{ label }}</div>
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.form-item {
+  .label {
+    line-height: 28px;
+
+    font-weight: 400;
+    font-size: 14px;
+    text-align: right;
+    color: white;
+    width: 85px;
+    -webkit-box-flex: 0;
+    -ms-flex: none;
+    flex: none;
+  }
+
+  .label.required::before {
+    content: '*';
+    color: #f53f3f;
+    vertical-align: -2px;
+  }
+
+  :deep(.form-item-content) {
+    flex: auto;
+  }
+}
+</style>
