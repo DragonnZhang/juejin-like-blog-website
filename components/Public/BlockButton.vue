da702ca872b1a1c4f291df2926ce3077a46be6c7
DragonnZhang <731557579@qq.com>
2023-11-01 13:50:23 +0800

    accomplished component

--- components/Public/BlockButton.vue
+++ components/Public/BlockButton.vue
@@ -1,13 +1,32 @@
 <script setup lang="ts">
 defineProps<{
   type: 'line' | 'primary'
+  size?: 'medium'
 }>()
 </script>
 
 <template>
-  <button>
+  <button :class="[type, size]">
     <slot></slot>
   </button>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.line {
+  background-color: #fff;
+  border: 1px solid #2986ff;
+  border-color: #1e80ff;
+  color: #1e80ff;
+}
+
+.primary {
+  background-color: #1e80ff;
+}
+
+.medium {
+  height: 32px;
+  padding: 2px 16px;
+  font-size: 14px;
+  line-height: 22px;
+}
+</style>
