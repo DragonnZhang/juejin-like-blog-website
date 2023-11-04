443e77da7c29e504aaaa181e52fbfb3d71110a51
DragonnZhang <731557579@qq.com>
2023-11-04 20:53:45 +0800

    extract logic from uploadarticle to a public component uploadFile

--- components/Editor/EditorMain.vue
+++ components/Editor/EditorMain.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import UploadArticle from '~/components/Editor/UploadArticle.vue'
+import UploadFile from '~/components/Public/UploadFile.vue'
 import { useArticle } from '~/composables/states'
 import { Editor } from '@bytemd/vue-next'
 import 'bytemd/dist/index.css'
@@ -20,7 +20,9 @@ function handleChange(v: string) {
 <template>
   <div class="main">
     <Editor :plugins="plugins" :value="article" @change="handleChange" :locale="zhHans" />
-    <UploadArticle />
+    <ClientOnly>
+      <UploadFile id="upload-markdown" v-model="article" type="md" />
+    </ClientOnly>
   </div>
 </template>
 
---
+++ components/Public/UploadFile.vue
@@ -0,0 +1,71 @@
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue: string
+  type: 'md' | 'image'
+}>()
+const emit = defineEmits(['update:modelValue'])
+
+// compute the type input accepts
+const accept = computed(() => {
+  const strategy = {
+    md: '.md',
+    image: 'image/*'
+  }
+
+  return strategy[props.type]
+})
+
+function readFileToString(file: File) {
+  return new Promise((resolve) => {
+    const reader = new FileReader()
+
+    reader.onload = function (e: Event) {
+      const arrayBuffer = (e.target as FileReader).result
+
+      // arrayBuffer to string
+      const uint8Array = new Uint8Array(arrayBuffer as ArrayBuffer)
+      const decoder = new TextDecoder()
+      const s = decoder.decode(uint8Array)
+
+      resolve(s)
+    }
+
+    reader.readAsArrayBuffer(file)
+  })
+}
+
+function readFileToUrl(file: File) {
+  return new Promise((resolve) => {
+    const reader = new FileReader()
+
+    reader.onload = function (e: Event) {
+      const url = (e.target as FileReader).result
+      resolve(url)
+    }
+
+    reader.readAsDataURL(file)
+  })
+}
+
+async function handleFileChange(e: Event) {
+  let file = (e.target as HTMLInputElement).files![0]
+
+  if (!file) return
+
+  try {
+    if (props.type === 'md') {
+      const value = await readFileToString(file)
+      emit('update:modelValue', value)
+    } else if (props.type === 'image') {
+      const url = await readFileToUrl(file)
+      emit('update:modelValue', url)
+    }
+  } catch (err) {
+    console.log('文件读取出错:', err)
+  }
+}
+</script>
+
+<template>
+  <input type="file" :oninput="handleFileChange" :accept="accept" v-show="false" />
+</template>
