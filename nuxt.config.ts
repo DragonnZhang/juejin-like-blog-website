d5c8d80405d8684ee50684cb1d66bb308bd9dbe7
DragonnZhang <731557579@qq.com>
2024-02-16 22:47:29 +0800

    perf: add http cache

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,5 +9,22 @@ export default defineNuxtConfig({
   },
   vue: {
     defineModel: true
+  },
+  routeRules: {
+    '/': {
+      static: true,
+      swr: 120
+    },
+    '/post/**': { swr: 10 },
+    '/editor': {
+      static: true,
+      swr: 1200
+    },
+    '/assets/**': {
+      headers: { 'cache-control': 's-maxage=1200' }
+    },
+    '/public/**': {
+      headers: { 'cache-control': 's-maxage=31536000' }
+    }
   }
 })
