851706adcb1d232cab6bf5095a3f9e8546b80ae1
DragonnZhang <731557579@qq.com>
2024-02-25 20:33:22 +0800

    fix: remove sentry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nuxt-multi-cache', '@nuxtjs/sentry'],
+  modules: ['nuxt-multi-cache'],
   multiCache: {
     component: {
       enabled: true
     }
-  },
-  sentry: {
-    dsn: 'https://13568e5ecb31863d4e97602d96bc3528@o4506807408984064.ingest.sentry.io/4506807418552320'
   },
   devtools: { enabled: true },
   pages: true,
